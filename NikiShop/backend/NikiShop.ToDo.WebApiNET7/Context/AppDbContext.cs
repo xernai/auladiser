635eba2e802b13515df4b3cc2ba559ed9545a602
xernai <montcorona@gmail.com>
2024-10-14 15:56:38 -0600

    Add API projects NET 7 and 8 (#5)
    * Adding file with link to create Azure SQL database
    
    * Adding Weatherforecast Web API project
    
    * Adding HR schema script file
    
    * Adding DbContext to handle CRUD
    
    * Add database connection to Azure
    
    * Add MVC application to frontend
    
    * Add .gitignore file to projects
    
    * Delete unnecessary files phase 1
    
    * Delete unnecessary files phase 2
    
    * Reorder projects related to NikiShop
    
    * Add API projects versions NET  7 and 8
    
    ---------
    
    Co-authored-by: Xabier Montez <montcouronne@hotmail.com>

---
+++ NikiShop/backend/NikiShop.ToDo.WebApiNET7/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NikiShop.ToDo.WebApiNET7.Models;
+
+namespace NikiShop.ToDo.WebApiNET7.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<ToDoList> ToDoList { get; set; }
+    }
+}
---
+++ NikiShop/backend/NikiShop.ToDo.WebApiNET8/Models/ToDoList.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NikiShop.ToDo.WebApiNET8.Models
+{
+    public class ToDoList
+    {
+        [Key]
+        public int ToDoListId { get; set; }
+        public string Name { get; set; }
+    }
+}
