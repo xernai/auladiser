ff809433627a38772618b76aef5af232dcaf060b
xernai <montcorona@gmail.com>
2024-10-13 21:20:15 -0600

    Adding file with link to create Azure SQL database (#4)
    * Adding file with link to create Azure SQL database
    
    * Adding Weatherforecast Web API project
    
    * Adding HR schema script file
    
    * Adding DbContext to handle CRUD
    
    * Add database connection to Azure
    
    * Add MVC application to frontend
    
    * Add .gitignore file to projects
    
    * Delete unnecessary files phase 1
    
    * Delete unnecessary files phase 2
    
    * Reorder projects related to NikiShop
    
    ---------
    
    Co-authored-by: Xabier Montez <montcouronne@hotmail.com>

---
+++ NikiShop/backend/NikiShop.ToDo.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NikiShop.ToDo.WebApi.Context;
+using NikiShop.ToDo.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NikiShop.ToDo.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly AppDbContext _context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,
+            AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult<IEnumerable<ToDoList>> GetAllToDoList()
+        {
+            // TODO: agregar el DbSet en el AppDbContext
+            var todoList = _context.ToDoList.ToList();
+            return Ok(todoList);
+        }
+    }
+}
