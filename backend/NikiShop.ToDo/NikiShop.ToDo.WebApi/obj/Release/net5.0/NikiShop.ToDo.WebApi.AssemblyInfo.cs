2bcf49cc44970ca677d7410268fc8667c17e269a
Xabier Montez <montcouronne@hotmail.com>
2024-10-13 20:50:07 -0600

    Add MVC application to frontend

--- backend/NikiShop.ToDo/NikiShop.ToDo.WebApi/obj/Release/net5.0/NikiShop.ToDo.WebApi.AssemblyInfo.cs
+++ backend/NikiShop.ToDo/NikiShop.ToDo.WebApi/obj/Release/net5.0/NikiShop.ToDo.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NikiShop.ToDo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50433b59b0b68c70b3e334780db2bf7758c907ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3d090c99ff9ea908f7af6d3e9d5119c29730e2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NikiShop.ToDo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NikiShop.ToDo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ frontend/NikiShop.ToDo.MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NikiShop.ToDo.MvcClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NikiShop.ToDo.MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ToDo()
+        {
+            // TODO: usar la clase HttpClient de .NET para conectarse a la API
+            // var baseURL = "http://localhost:44323/"
+            // var baseURL = baseURL + "WeatherForecast/GetAllToDoList"
+            // var httpClient = new HttpClient();
+
+            // httpClient deserializarlo a un Modelo = todoList
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
